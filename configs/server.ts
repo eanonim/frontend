fa10e7b96437be23c6b1874130f306828b82157e
GMELUM <gmelum@vk.com>
2025-02-17 09:05:28 +0300

    swap host to dev

--- configs/server.ts
+++ configs/server.ts
@@ -1,4 +1,5 @@
-export let HOST = "eanonim-backend-9a6d.twc1.net"
+// export let HOST = "eanonim-backend-9a6d.twc1.net"
+export let HOST = "dev.elum.app"
 
 // if (import.meta.env.MODE === "development") {
 //   HOST = "dev.elum.app"

daff804ac5b4b20286597b6db51f22a13ff43396
Егор <root@apiteam.ru>
2025-02-23 23:53:44 +0300

    Edit ATOM

--- configs/server.ts
+++ configs/server.ts
@@ -1,5 +1,5 @@
-// export let HOST = "anonim.elum.app" //"eanonim-backend-9a6d.twc1.net"
-export let HOST = "dev.elum.app"
+export let HOST = "anonim.elum.app" //"eanonim-backend-9a6d.twc1.net"
+// export let HOST = "dev.elum.app"
 
 // if (import.meta.env.MODE === "development") {
 //   HOST = "dev.elum.app"

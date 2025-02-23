4d8ff0e63a38f5496c7dc4a948633dced3d45496
Егор <root@apiteam.ru>
2025-02-23 05:48:21 +0300

    Edit system caching messages

--- src/engine/utils/getFullDate.ts
+++ src/engine/utils/getFullDate.ts
@@ -4,9 +4,9 @@ const getFullDate = (date: Date): string => {
   }
 
   const year = date.getFullYear()
-  const month = date.getMonth()
-  const day = date.getDate()
-  return `${year}-${month + 1}-${day}`
+  const month = String(date.getMonth() + 1).padStart(2, "0")
+  const day = String(date.getDate()).padStart(2, "0")
+  return `${year}-${month}-${day}`
 }
 
 export default getFullDate

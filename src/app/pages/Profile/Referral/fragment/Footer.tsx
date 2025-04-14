5f741d9a7360cb7afec6dc7992c05449187e6e07
Егор <57842677+xuserz@users.noreply.github.com>
2025-04-14 23:50:32 +0300

    Edit referral

--- src/app/pages/Profile/Referral/fragment/Footer.tsx
+++ src/app/pages/Profile/Referral/fragment/Footer.tsx
@@ -2,7 +2,7 @@ import { Button, FixedLayout, Title } from "components"
 import loc from "engine/languages"
 import { copyText } from "@minsize/utils"
 
-import { type JSX, type Component } from "solid-js"
+import { type JSX, type Component, createSignal } from "solid-js"
 import { URL_APP } from "root/configs"
 import { useAtom } from "engine/modules/smart-data"
 import { USER_ATOM } from "engine/state"
@@ -12,16 +12,38 @@ interface Footer extends JSX.HTMLAttributes<HTMLDivElement> {}
 const Footer: Component<Footer> = (props) => {
   const [lang] = loc()
   const [user] = useAtom(USER_ATOM)
+  const [status, setStatus] = createSignal<undefined | "success" | "error">(
+    undefined,
+  )
+  let timer: NodeJS.Timeout
 
   const handlerCopy = () => {
-    copyText(`${URL_APP}?startapp=ref_${user?.referrer_code}`)
+    const status = copyText(`${URL_APP}?startapp=ref_${user?.referrer_code}`)
+    setStatus(status ? "success" : "error")
+
+    clearTimeout(timer)
+
+    timer = setTimeout(() => {
+      setStatus(undefined)
+    }, 1500)
   }
 
   return (
     <FixedLayout safe background={"section_bg_color"} position={"bottom"}>
       <Button.Group>
         <Button.Group.Container>
-          <Button onClick={handlerCopy} size={"large"} stretched>
+          <Button
+            onClick={handlerCopy}
+            size={"large"}
+            stretched
+            appearance={
+              status() === undefined
+                ? "accent"
+                : status() === "success"
+                ? "green"
+                : "red"
+            }
+          >
             <Button.Container>
               <Title>{lang("copy")}</Title>
             </Button.Container>

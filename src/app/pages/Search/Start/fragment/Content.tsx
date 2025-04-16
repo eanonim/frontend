583727431874f490a85de49ab39be3657e680441
Егор <57842677+xuserz@users.noreply.github.com>
2025-04-16 17:08:52 +0300

    test opacity

--- src/app/pages/Search/Start/fragment/Content.tsx
+++ src/app/pages/Search/Start/fragment/Content.tsx
@@ -3,7 +3,7 @@ import { chatSearchEnd } from "engine/api"
 import loc from "engine/languages"
 import { backPage } from "router"
 
-import { type JSX, type Component } from "solid-js"
+import { type JSX, type Component, onMount, onCleanup } from "solid-js"
 
 interface Content extends JSX.HTMLAttributes<HTMLDivElement> {}
 
@@ -15,6 +15,29 @@ const Content: Component<Content> = (props) => {
     backPage()
   }
 
+  // onMount(() => {
+  //   var div1 = document.getElementById("telegram-notification-content")
+  //   var container2 = document.getElementById("ad-banner")
+
+  //   // Перемещаем div1 в container2
+  //   if (div1) {
+  //     container2?.appendChild(div1)
+  //     div1.style.bottom = "0px"
+  //     div1.style.left = "0px"
+  //     div1.style.right = "0px"
+  //     div1.style.display = "block"
+  //     div1.style.position = "relative"
+  //   }
+
+  //   onCleanup(() => {
+  //     if (div1) {
+  //       var container = document.body
+  //       container?.appendChild(div1)
+  //       div1.style.display = "none"
+  //     }
+  //   })
+  // })
+
   return (
     <Plug full>
       <Plug.Container>
@@ -23,7 +46,17 @@ const Content: Component<Content> = (props) => {
         </Plug.Icon>
       </Plug.Container>
       <Plug.Action stretched>
-        <div style={{ padding: "10px" }} id={"ad-banner"} />
+        <div
+          style={{
+            display: "block",
+            width: "100%",
+            height: "100%",
+            position: "relative",
+            "--telegram-notification-content": "block !important",
+            "box-sizing": "border-box",
+          }}
+          id={"ad-banner"}
+        />
       </Plug.Action>
       <Plug.Action stretched>
         <Button

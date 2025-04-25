40e2642c70aac980199a4b04c8cb4d5eff2d9950
Егор <57842677+xuserz@users.noreply.github.com>
2025-04-25 17:32:39 +0300

    mone

--- configs/server.ts
+++ configs/server.ts
@@ -1,5 +1,5 @@
 export let HOST = "anback.elum.su" //"eanonim-backend-9a6d.twc1.net"
-export let HOST_CDN = "acdn.elum.app"
+export let HOST_CDN = "ancdn.elum.su"
 // export let HOST = "dev.elum.app"
 
 // if (import.meta.env.MODE === "development") {
